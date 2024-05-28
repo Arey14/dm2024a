eec42d1c18bb7908ea003051022e2c5c7fca375d
Augusto Rey <augustorey_@hotmail.com>
2024-05-28 01:31:54 -0300

    workflow

--- src/ambiente/z301_GrabarMisSemillas.r
+++ src/ambiente/z301_GrabarMisSemillas.r
@@ -4,11 +4,11 @@
 require( "data.table" )
 
 # reemplazar aqui por SUS semillas 
-mis_semillas <- c(102191, 200177, 410551, 552581, 892237)
+mis_semillas <- c(734471, 734473, 734477, 734479, 734497, 734537, 734543, 734549, 734557, 734567, 734627,734647,734653,734659, 734663, 734687, 734693, 734707, 734717, 734729)
 
 tabla_semillas <- as.data.table(list( semilla = mis_semillas ))
 
 fwrite( tabla_semillas,
-    file = "~/buckets/b1/datasets/mis_semillas.txt",
+    file = "~/Desktop/ITBA/Mineria de Datos/dm2024a/src/workflow-01/Modelo Propios/buckets/b1/dataset/mis_semillas.txt",
     sep = "\t"
 )
